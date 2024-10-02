8100feb23b8133714f4eb189c1378fc52df26b44
Yiming0576 <Yiming0576@gmail.com>
2024-10-02 14:33:00 -0400

    update the code and fix bugs

--- FullNameParser/FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
+++ FullNameParser/FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -11,37 +11,44 @@ public class FullNameParser {
     }
 
     /**
-     * Ask user to enter full name. by using the split and trim method to get the first name, last name, middle name.
-     *
+     * Ask user to enter full name.
+     * by using the split and trim method to get the first name, last name, middle name.
      */
     public void nameParser() {
-
-
-
         try (Scanner scanner = new Scanner(System.in)){
 
             System.out.println("Please enter your full name: ");
-            System.out.print("Last name: ");
             String fullName = scanner.nextLine();
+            String[] names = fullName.trim().split("\\s+");
 
-            String[] names = fullName.trim().split(" ");
             int length = names.length;
+            String firstName = "";
+            String lastName = "";
+            String middleName = "";
 
+            for (String name: names) {
+                
+            }
+
+            System.out.println(length);
             if (length < 0) {
                 System.out.println("Please enter your name");
-            } else if (length >= 3){
-                String firstName = names[0];
-                String middleName = names[1].equals("") ? "(None)" : names[1];
-                String lastName = names[2];
-//                String suffix = names[3];
+            } else if (length == 3){
+                firstName = names[0];
+                middleName = names[1].isBlank() ? "(None)" : names[1];
+                lastName = names[2];
+            } else if (length == 2) {
+                firstName = names[0];
+                lastName = names[1];
 
-                System.out.print("Last name  : " + lastName + "\n");
-                System.out.print("Middle name: " + middleName+ "\n");
-                System.out.print("First name : " + firstName  + "\n");
-//                System.out.print("Suffix     : " + suffix + "\n");
-
-                System.out.print("full name  : " + fullName + "\n");
             }
+
+
+
+            System.out.print("Last name  : " + lastName + "\n");
+            System.out.print("Middle name: " + middleName+ "\n");
+            System.out.print("First name : " + firstName  + "\n");
+            System.out.print("full name  : " + fullName + "\n");
 
 
         } catch (Exception e) {
--- fullnamegenerator/src/main/java/com/pluralsight/FullnameGenerator.java
+++ fullnamegenerator/src/main/java/com/pluralsight/FullnameGenerator.java
@@ -7,8 +7,70 @@ public class FullnameGenerator {
     public static void main(String[] args) {
 
         FullnameGenerator fullnameGenerator = new FullnameGenerator();
-        fullnameGenerator.nameGenerate();
+        fullnameGenerator.nameGenerate1();
+        fullnameGenerator.nameGenerate2();
+        fullnameGenerator.nameGenerate3();
+    }
 
+    private void nameGenerate3() {
+        StringBuilder fullNameWithStringBuilder = new StringBuilder();
+
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Please enter your name: ");
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.print("Middle name: ");
+            String middleName = scanner.nextLine();
+
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+
+            System.out.print("Suffix: ");
+            String suffix = scanner.nextLine();
+
+
+
+            String fullName = firstName + (middleName.isBlank()? "" : " " + middleName + " ") + lastName + (suffix.isBlank()? "" : ", " + suffix);
+
+            System.out.println("Full name: " + fullName);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void nameGenerate2() {
+        StringBuilder fullNameWithStringBuilder = new StringBuilder();
+
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Please enter your name: ");
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.print("Middle name: ");
+            String middleName = scanner.nextLine();
+
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+
+            System.out.print("Suffix: ");
+            String suffix = scanner.nextLine();
+
+            middleName += middleName.isBlank()? "" : " " + middleName + " ";
+            suffix += suffix.isBlank()? "" : ", " + suffix;
+
+            String fullName = firstName + middleName + lastName + suffix;
+
+            System.out.println("Full name: " + fullName);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
@@ -16,8 +78,9 @@ public class FullnameGenerator {
      * Ask user to enter their firstName, middle name, last name, and suffix.
      *
      */
-    public void nameGenerate() {
+    public void nameGenerate1() {
 
+        StringBuilder fullNameWithStringBuilder = new StringBuilder();
 
 
         try (Scanner scanner = new Scanner(System.in)){
@@ -34,14 +97,21 @@ public class FullnameGenerator {
 
             System.out.print("Suffix: ");
             String suffix = scanner.nextLine();
+            String fullName = "";
 
-            String fullName = suffix.equals("") ? lastName + " " + middleName + " " + firstName:  lastName + " " + middleName + " " + firstName + ", " + suffix;
 
-            StringBuilder fullNameWithStringBuilder = new StringBuilder();
-            String nameWithStringBuilder = fullNameWithStringBuilder.append(lastName).append(" ").append(middleName).append(" ").append(firstName).toString();
+//          one way
+            if (suffix.isBlank() && middleName.isBlank()) {
+                fullName = firstName + " " + lastName;
+            } else {
+                if (suffix.isBlank()) {
+                    fullName = firstName + " " + middleName + " " + lastName;
+                } else if (middleName.isBlank()) {
+                    fullName = firstName + " " + lastName + ", " + suffix;
+                } else {
+                    fullName = firstName + " " + middleName + " " + lastName + ", " + suffix;
 
-            if (!suffix.equals("")) {
-                nameWithStringBuilder = fullNameWithStringBuilder.append(", ").append(suffix).toString();
+                }
             }
 
             System.out.print("Last name  : " + lastName + "\n");
@@ -50,7 +120,7 @@ public class FullnameGenerator {
             System.out.print("Suffix     : " + suffix + "\n");
 
             System.out.print("full name  : " + fullName + "\n");
-            System.out.println("StringBuilder Full name: " + nameWithStringBuilder);
+//            System.out.println("StringBuilder Full name: " + nameWithStringBuilder);
 
 
         } catch (Exception e) {
